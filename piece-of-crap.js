6276a11b002d7110689ca565b8f64fd357948104
themirrazz <89490659+themirrazz@users.noreply.github.com>
2022-04-27 21:19:31 -0700

    Create piece-of-crap.js

---
+++ piece-of-crap.js
@@ -0,0 +1,16 @@
+(function(){var sw=new w96.StandardWindow({
+    body:'bruh this is stupid test',
+    title:'piece-of-crap.exe',
+});
+sw.show()
+setTimeout(function(){sw.registerAppBar()
+var aw = function aw(){
+    sw.setControlBoxStyle("WS_CBX_MINCLOSE");
+    setTimeout(yay,250);
+}
+                      var yay=function yay(){
+    sw.setControlBoxStyle("WS_CBX_MINMAXCLOSE");
+    setTimeout(aw,250);
+}
+setTimeout(yay,1000);
+                     },1000);})()
